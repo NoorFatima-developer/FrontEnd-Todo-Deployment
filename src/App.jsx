7f2172e70955e60eaed437ceac56b3110cd35633
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 17:40:04 +0500

    code is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router, Routes} from "react-router-dom";
 
 
 function App() {
 
   return (
     <Router>
-
+        <Routes>
+          
+        </Routes>
     </Router>
   )
 }
