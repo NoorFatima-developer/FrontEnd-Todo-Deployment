3cc6650f277fcba22cd4403f300542e27152d450
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 23:35:41 +0500

    code is added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 function Register() {
+
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        // add form data to database or API here
+        console.log('Form submitted')
+    }
+
+
+
   return (
     <div>
         <div className='login'>
       <section>
-        <form>
-            <input type='text' placeholder='Name'>Name</input>
+        <form onSubmit={submitHandler}>
+            <input type='text' placeholder='Name'/>
             <input type='text' placeholder='Email' />
             <input type='password' placeholder='Password' />
             <button type='submit'>Register</button>
