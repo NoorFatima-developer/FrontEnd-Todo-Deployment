aa316a46dfbcb0dd39836eda84a258509bdd342f
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-26 04:13:45 +0500

    code is added

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import "./styles/app.scss"
 import App from './App.jsx'
 
+
+// yahan pr mai backend ka url save kr ri o variable mai ta k hr jagah mai jahan pr b use kro mjy pura url na 
+// dena pry bs mjy variable ka nam e dena pry...
+
+export const server = "https://backend-todo-deploy.vercel.app/api/v1/users/me";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
