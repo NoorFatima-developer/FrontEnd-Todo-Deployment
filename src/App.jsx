ae986408f0be9ae2f53165885da610314d05d2f3
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 22:38:30 +0500

    code is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx"
 import Header from "./components/Header.jsx";
+import Profile from "./pages/Profile.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 
 function App() {
 
@@ -11,7 +14,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/profile" element={<Profile/>} />
           <Route path="/login" element={<Login/>} />
-          <Route path="/register" element={<Login/>} />
+          <Route path="/register" element={<Register/>} />
         </Routes>
     </Router>
   )
