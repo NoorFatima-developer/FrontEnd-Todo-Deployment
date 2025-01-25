5eb89ae65368a747aa81b3fd2e84586c89208ad0
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 22:25:06 +0500

    code is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx"
+import Header from "./components/Header.jsx";
 
 function App() {
 
   return (
     <Router>
         <Routes>
+          <Header/>
           <Route path="/" element={<Home/>} />
         </Routes>
     </Router>
