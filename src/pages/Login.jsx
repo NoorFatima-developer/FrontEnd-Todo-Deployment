d4e1f030d131d62df710660a1cdbcdc75c66db35
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 23:12:55 +0500

    code is added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
-    <div>
-      Login
+    <div className='login'>
+      <section>
+        <form>
+            <input type='text' placeholder='Email' />
+            <input type='password' placeholder='Password' />
+            <button type='submit'>Login</button>
+            <h4>Or</h4>
+            <Link to={"/register"}>Sign Up</Link>
+        </form>
+      </section>
     </div>
   )
 }
