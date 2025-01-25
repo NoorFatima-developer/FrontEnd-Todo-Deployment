193fde38cbeadd8570baf8e8343a2cb091981976
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-25 23:19:21 +0500

    code is added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 function Register() {
   return (
     <div>
-        <div className='register'>
+        <div className='login'>
       <section>
         <form>
             <input type='text' placeholder='Email' />
             <input type='password' placeholder='Password' />
             <button type='submit'>Register</button>
             <h4>Or</h4>
-            <Link to={"/register"}>Sign Up</Link>
+            <Link to={"/register"}>Sign In</Link>
         </form>
       </section> 
     </div>
