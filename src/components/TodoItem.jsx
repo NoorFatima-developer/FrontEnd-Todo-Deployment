a4575dc183833c206684025633179093703480c4
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-28 22:31:24 +0500

    code is added

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TodoItem = () => {
+  return (
+    <div className='todo'>
+      
+      <div>
+        <h4></h4>
+        <p></p>
+      </div>
+      <div></div>
+    </div>
+  )
+}
+
+export default TodoItem
