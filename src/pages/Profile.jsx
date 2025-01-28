127ec0566fbecd41b2de104dcd102401ee1f91c1
NoorFatima-developer <nf99noorfatima@gmail.com>
2025-01-28 16:09:10 +0500

    code is added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { Context } from '../main';
+import Loader from '../components/Loader';
 
 const Profile = () => {
   const { isAuthenticated, loading, user } = useContext(Context);
@@ -7,12 +8,14 @@ const Profile = () => {
 
       //not getting user data...
       // console.log(user);
-    
+   return(
+     loading ? <Loader/> :(
     <div>
       <h1>{user?.name}</h1>
       <p>{user?.email}</p>
     </div>
- 
+   )
+  )
 };
 
 export default Profile
